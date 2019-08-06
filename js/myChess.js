a1e29acc17d25940b0ce4b71e82d9c77c5ea9e62
alexis <alexis.carbillet@mines-ales.org>
2019-08-06 09:44:29 +0200

    Update myChess.js

--- js/myChess.js
+++ js/myChess.js
@@ -2,11 +2,28 @@ var init = function() {
 
 var board = null
 var game = new Chess()
+var whiteSquareGrey = '#a9a9a9'
+var blackSquareGrey = '#696969'
+
+function removeGreySquares () {
+  $('#board .square-55d63').css('background', '')
+}
+
+function greySquare (square) {
+  var $square = $('#board .square-' + square)
+
+  var background = whiteSquareGrey
+  if ($square.hasClass('black-3c85d')) {
+    background = blackSquareGrey
+  }
+
+  $square.css('background', background)
+}
+
 
 function onDragStart (source, piece, position, orientation) {
   // do not pick up pieces if the game is over
   if (game.game_over()) return false
-
   // only pick up pieces for White
   if (piece.search(/^b/) !== -1) return false
 }
@@ -23,6 +40,7 @@ function makeRandomMove () {
 }
 
 function onDrop (source, target) {
+  removeGreySquares()
   // see if the move is legal
   var move = game.move({
     from: source,
@@ -37,6 +55,29 @@ function onDrop (source, target) {
   window.setTimeout(makeRandomMove, 250)
 }
 
+function onMouseoverSquare (square, piece) {
+  // get list of possible moves for this square
+  var moves = game.moves({
+    square: square,
+    verbose: true
+  })
+
+  // exit if there are no moves available for this square
+  if (moves.length === 0) return
+
+  // highlight the square they moused over
+  greySquare(square)
+
+  // highlight the possible squares for this piece
+  for (var i = 0; i < moves.length; i++) {
+    greySquare(moves[i].to)
+  }
+}
+
+function onMouseoutSquare (square, piece) {
+  removeGreySquares()
+}
+
 // update the board position after the piece snap
 // for castling, en passant, pawn promotion
 function onSnapEnd () {
@@ -48,6 +89,8 @@ var config = {
   position: 'start',
   onDragStart: onDragStart,
   onDrop: onDrop,
+  onMouseoutSquare: onMouseoutSquare,
+  onMouseoverSquare: onMouseoverSquare,
   onSnapEnd: onSnapEnd
 }
 board = ChessBoard('board', config)
