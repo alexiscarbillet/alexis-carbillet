eb2d60214eae466dae3ea0e8082ca5d4fe69cc2d
ALEXIS CARBILLET <alexiscarbillet@users.noreply.github.com>
2025-08-20 17:30:56 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -355,7 +355,7 @@ export default function ExperienceTimeline() {
             <div className="card">
               <div className="card-content">
                 <p className="title" style={{ wordBreak: 'normal' }}>CKA Practice: ConfigMaps and Secrets</p>
-                <button className="button is-primary is-outlined"><a href="https://killercoda.com/alexis-carbillet/scenario/configmap-secrets" target="_blank">Website</a></button>
+                <button className="button is-primary is-outlined"><a href="https://killercoda.com/alexis-carbillet/course/CKA/configmap-secrets" target="_blank">Website</a></button>
               </div>
             </div>
           </div>
@@ -363,7 +363,7 @@ export default function ExperienceTimeline() {
             <div className="card">
               <div className="card-content">
                 <p className="title" style={{ wordBreak: 'normal' }}>CKA Practice: Horizontal Pod Autoscaler (HPA)</p>
-                <button className="button is-primary is-outlined"><a href="https://killercoda.com/alexis-carbillet/scenario/HPA" target="_blank">Website</a></button>
+                <button className="button is-primary is-outlined"><a href="https://killercoda.com/alexis-carbillet/course/CKA/HPA" target="_blank">Website</a></button>
               </div>
             </div>
           </div>
@@ -371,7 +371,7 @@ export default function ExperienceTimeline() {
             <div className="card">
               <div className="card-content">
                 <p className="title" style={{ wordBreak: 'normal' }}>CKA Practice: Network Policies</p>
-                <button className="button is-primary is-outlined"><a href="https://killercoda.com/alexis-carbillet/scenario/network-policies" target="_blank">Website</a></button>
+                <button className="button is-primary is-outlined"><a href="https://killercoda.com/alexis-carbillet/course/CKA/network-policies" target="_blank">Website</a></button>
               </div>
             </div>
           </div>
@@ -383,7 +383,7 @@ export default function ExperienceTimeline() {
             <div className="card">
               <div className="card-content">
                 <p className="title" style={{ wordBreak: 'normal' }}>CKA Practice: Node Maintenance</p>
-                <button className="button is-primary is-outlined"><a href="https://killercoda.com/alexis-carbillet/scenario/node-maintenance" target="_blank">Website</a></button>
+                <button className="button is-primary is-outlined"><a href="https://killercoda.com/alexis-carbillet/course/CKA/node-maintenance" target="_blank">Website</a></button>
               </div>
             </div>
           </div>
@@ -391,7 +391,7 @@ export default function ExperienceTimeline() {
             <div className="card">
               <div className="card-content">
                 <p className="title" style={{ wordBreak: 'normal' }}>CKA Practice: Persistent Volumes and PVC</p>
-                <button className="button is-primary is-outlined"><a href="https://killercoda.com/alexis-carbillet/scenario/pv-pvc" target="_blank">Website</a></button>
+                <button className="button is-primary is-outlined"><a href="https://killercoda.com/alexis-carbillet/course/CKA/pv-pvc" target="_blank">Website</a></button>
               </div>
             </div>
           </div>
@@ -399,7 +399,7 @@ export default function ExperienceTimeline() {
             <div className="card">
               <div className="card-content">
                 <p className="title" style={{ wordBreak: 'normal' }}>CKA Practice: Pod Debugging</p>
-                <button className="button is-primary is-outlined"><a href="https://killercoda.com/alexis-carbillet/scenario/pod-debugging" target="_blank">Website</a></button>
+                <button className="button is-primary is-outlined"><a href="https://killercoda.com/alexis-carbillet/course/CKA/pod-debugging" target="_blank">Website</a></button>
               </div>
             </div>
           </div>
@@ -411,7 +411,7 @@ export default function ExperienceTimeline() {
             <div className="card">
               <div className="card-content">
                 <p className="title" style={{ wordBreak: 'normal' }}>CKA Practice: RBAC (Role-Based Access Control)</p>
-                <button className="button is-primary is-outlined"><a href="https://killercoda.com/alexis-carbillet/scenario/rbac" target="_blank">Website</a></button>
+                <button className="button is-primary is-outlined"><a href="https://killercoda.com/alexis-carbillet/course/CKA/rbac" target="_blank">Website</a></button>
               </div>
             </div>
           </div>
@@ -419,7 +419,7 @@ export default function ExperienceTimeline() {
             <div className="card">
               <div className="card-content">
                 <p className="title" style={{ wordBreak: 'normal' }}>CKA Practice: Rolling Updates & Rollbacks</p>
-                <button className="button is-primary is-outlined"><a href="https://killercoda.com/alexis-carbillet/scenario/rolling-updates" target="_blank">Website</a></button>
+                <button className="button is-primary is-outlined"><a href="https://killercoda.com/alexis-carbillet/course/CKA/rolling-updates" target="_blank">Website</a></button>
               </div>
             </div>
           </div>
@@ -427,7 +427,7 @@ export default function ExperienceTimeline() {
             <div className="card">
               <div className="card-content">
                 <p className="title" style={{ wordBreak: 'normal' }}>CKA Practice: Troubleshooting a CrashLoopBackOff Pod</p>
-                <button className="button is-primary is-outlined"><a href="https://killercoda.com/alexis-carbillet/scenario/CrashLoopBackOff" target="_blank">Website</a></button>
+                <button className="button is-primary is-outlined"><a href="https://killercoda.com/alexis-carbillet/course/CKA/CrashLoopBackOff" target="_blank">Website</a></button>
               </div>
             </div>
           </div>
@@ -435,7 +435,7 @@ export default function ExperienceTimeline() {
             <div className="card">
               <div className="card-content">
                 <p className="title" style={{ wordBreak: 'normal' }}>CKA Practice: Service Not Routing Traffic</p>
-                <button className="button is-primary is-outlined"><a href="https://killercoda.com/alexis-carbillet/scenario/service" target="_blank">Website</a></button>
+                <button className="button is-primary is-outlined"><a href="https://killercoda.com/alexis-carbillet/course/CKA/service" target="_blank">Website</a></button>
               </div>
             </div>
           </div>
