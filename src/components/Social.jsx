f3e4bc889085b79176d4174358a23c5d9126d278
ALEXIS CARBILLET <49431201+alexiscarbillet@users.noreply.github.com>
2026-09-04 21:19:43 -0400

    Update Social.jsx

--- src/components/Social.jsx
+++ src/components/Social.jsx
@@ -8,44 +8,6 @@ export default function Social() {
   return (
     <div className="container">
       <div className="team-member">
-
-        {/* First row of icons - left aligned with top margin */}
-        <div className="columns is-mobile is-flex mt-5 mb-3">
-          <div className="column is-narrow">
-            <a href="https://coding.alexis-carbillet.com" target="_blank" rel="noopener noreferrer">
-              <FontAwesomeIcon icon={faCode} size="lg" />
-            </a>
-          </div>
-          <div className="column is-narrow">
-            <a href="https://electricity.alexis-carbillet.com" target="_blank" rel="noopener noreferrer">
-              <FontAwesomeIcon icon={faBolt} size="lg" />
-            </a>
-          </div>
-          <div className="column is-narrow">
-            <a href="https://alexis-carbillet.com" target="_blank" rel="noopener noreferrer">
-              <FontAwesomeIcon icon={faHome} size="lg" />
-            </a>
-          </div>
-          <div className="column is-narrow">
-            <a href="https://cats.alexis-carbillet.com" target="_blank" rel="noopener noreferrer">
-              <FontAwesomeIcon icon={faPaw} size="lg" />
-            </a>
-          </div>
-          <div className="column is-narrow">
-            <a href="https://hardware.alexis-carbillet.com" target="_blank" rel="noopener noreferrer">
-              <FontAwesomeIcon icon={faServer} size="lg" />
-            </a>
-          </div>
-          <div className="column is-narrow">
-            <a href="https://blog.alexis-carbillet.com" target="_blank" rel="noopener noreferrer">
-              <FontAwesomeIcon icon={faNewspaper} size="lg" />
-            </a>
-          </div>
-        </div>
-
-        <hr style={{ width: "280px", border: "1px solid gray", margin: "1rem 0" }} />
-
-        {/* Second row of icons - left aligned */}
         <div className="columns is-mobile is-flex">
           <div className="column is-narrow">
             <a href="https://github.com/alexiscarbillet" target="_blank" rel="noopener noreferrer">
@@ -63,7 +25,6 @@ export default function Social() {
             </a>
           </div>
         </div>
-
       </div>
     </div>
   );
